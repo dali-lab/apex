ab646f26562e2dcdcf286956e789cb65d0756108
Armin Mahban <armin17@cs.dartmouth.edu>
2016-04-16 18:02:45 -0400

    detail whatever

--- Apex/Apex/MyTrips.swift
+++ Apex/Apex/MyTrips.swift
@@ -1,5 +1,5 @@
 //
-//  HomeFeedViewController.swift
+//  MyTrips.swift
 //  Apex
 //
 //  Created by ArminM on 4/16/16.
@@ -9,86 +9,14 @@
 import UIKit
 import Firebase
 
-
-
-// TRIP
-class TripClass {
+class MyTrips: UIViewController {
     
-    static let ref = Firebase(url: "https://apexdatabase.firebaseio.com/trips")
-    
-    // a Trip in the database MUST have these fields
-    var name: String = ""
-    var leaders: [String] = []
-    var maxMembers: Int = 0
-    var cost: Int = 0
-    var tags: [String] = []
-    var lat: CGFloat = 0.0
-    var long: CGFloat = 0.0
-    var description: String = ""
-    var startTime: Double = 0
-    var endTime: Double = 0
-    
-    // these fields are optional
-    var members: [String] = []
-
-    
-    
-    init(name: String, leaders: [String], maxMembers: Int, cost: Int, tags: [String], lat: CGFloat, long: CGFloat, members: [String], description: String, startTime: Double, endTime: Double) {
-        self.name = name
-        self.leaders = leaders
-        self.maxMembers = maxMembers
-        self.cost = cost
-        self.tags = tags
-        self.lat = lat
-        self.long = long
-        self.members = members
-        self.description = description
-        self.startTime = startTime
-        self.endTime = endTime
-    }
-    
-    func getRemainingSpots() -> Int {
-        return maxMembers - leaders.count - members.count
-    }
-    
-    class func getTrips(callback:(trips: [TripClass]) -> Void) {
-        ref.observeSingleEventOfType(.Value, withBlock: { snapshot in
-            var trips = [TripClass]()
-            print("getting trips: ")
-            for trip in snapshot.children {
-                print(trip.value.objectForKey("name") as! String)
-                
-                let name = trip.value.objectForKey("name") as! String
-                let leaders = trip.value.objectForKey("leaders") as! [String]
-                let maxMembers = trip.value.objectForKey("maxMembers") as! Int
-                let cost = trip.value.objectForKey("cost") as! Int
-                let tags = trip.value.objectForKey("tags") as! [String]
-                let lat = trip.value.objectForKey("lat") as! CGFloat
-                let long = trip.value.objectForKey("long") as! CGFloat
-                var members = trip.value.objectForKey("members") as? [String]
-                if members == nil {
-                    members = []
-                }
-                let description = trip.value.objectForKey("description") as! String
-                let startTime = trip.value.objectForKey("startTime") as! Double
-                let endTime = trip.value.objectForKey("endTime") as! Double
-                
-                trips.append(TripClass(name: name, leaders: leaders, maxMembers: maxMembers, cost: cost, tags: tags, lat: lat, long: long, members: members!, description: description, startTime: startTime, endTime: endTime))
-            }
-            callback(trips: trips)
-            }, withCancelBlock: { error in
-                print(error.description)
-        })
-    }
-
-}
-
-// UIViewController
-class HomeFeedViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var tripArr = [TripClass]()
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -108,8 +36,7 @@ class HomeFeedViewController: UIViewController {
         
         
     }
-
-
+    
     func myfunc(trips: [TripClass]) {
         tripArr = trips
         print("reloading database")
@@ -120,7 +47,7 @@ class HomeFeedViewController: UIViewController {
     
     func iconMapping(tag:String) -> String {
         
-
+        
         //used in change region
         let iconDict: [String:String] = [
             "Hiking" : "icon_hiking",
@@ -133,15 +60,7 @@ class HomeFeedViewController: UIViewController {
         
         return iconDict[tag]!
     }
-
-
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return tripArr.count
@@ -152,10 +71,9 @@ class HomeFeedViewController: UIViewController {
         return 1
     }
     
-    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("FeedCell", forIndexPath: indexPath) as! FeedTableViewCell
-
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyTripsCell", forIndexPath: indexPath) as! FeedTableViewCell
+        
         let trip = tripArr[indexPath.section]
         
         
@@ -172,12 +90,12 @@ class HomeFeedViewController: UIViewController {
         } else {
             cell.registration.text = cell.registration.text! + ", $" + String(trip.cost)
         }
-
+        
         cell.descriptionText.numberOfLines = 0
-
+        
         cell.descriptionText.text = trip.description
         
-//        cell.descriptionText.text = "1\n2\n3"
+        //        cell.descriptionText.text = "1\n2\n3"
         
         
         let startDate = NSDate(timeIntervalSince1970: trip.startTime)
@@ -214,5 +132,5 @@ class HomeFeedViewController: UIViewController {
         self.navigationController!.pushViewController(detailVC, animated: true)
     }
 
-    
+
 }
