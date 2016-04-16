75cc6fa2792afaf729ba13903df131b572b25668
Armin Mahban <armin17@cs.dartmouth.edu>
2016-04-16 16:46:12 -0400

    Expanding description

--- Apex/Apex/DetailViewController.swift
+++ Apex/Apex/DetailViewController.swift
@@ -23,8 +23,8 @@ class DetailViewController: UIViewController, MKMapViewDelegate {
     @IBOutlet weak var leader2Label: UILabel!
     @IBOutlet weak var leader3Image: UIImageView!
     @IBOutlet weak var leader3Label: UILabel!
+    @IBOutlet weak var textDescription: UITextView!
     
-
     @IBOutlet weak var mapView: MKMapView!
     
     @IBOutlet weak var slideshow: ImageSlideshow!
@@ -52,9 +52,17 @@ class DetailViewController: UIViewController, MKMapViewDelegate {
         }
         
         
-//        tripDescription.text = "lskdjf l;lskdj flkdjflsjf lksjasldkjf lakjsd lf;kajsd lfjs ldfkaj s;ldkfj slkdjf alskdjf a;lskdjf lskjdf laksjd flksjdf;lskdjf al;skdjf laksjdflaksdjflksj fl;aksdjf ;lksjf "
-//        
-//        tripDescription.requiredHeight()
+        textDescription.text = "lskdjf l;lskdj flkdjflsjf lksjasldkjf lakjsd lf;kajsd lfjs ldfkaj s;ldkfj slkdjf alskdjf a;lskdjf lskjdf laksjd flksjdf;lskdjf al;skdjf laksjdflaksdjflksj fl;aksdjf\n ksjdkf \n sdjfs\nsdjkfskd fj\nkdjsf skdj \nsdkfj sdkfj\nsdjkf sj\nsdkfj \nsdkfjs \n kjds f\ns dfkjsd \ns dkfjs d\nsdkf jsdkf d ;lksjf "
+        
+        let fixedWidth = textDescription.frame.size.width
+        textDescription.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.max))
+        let newSize = textDescription.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.max))
+        var newFrame = textDescription.frame
+        newFrame.size = CGSize(width: max(newSize.width, fixedWidth), height: newSize.height)
+        textDescription.frame = newFrame
+        textDescription.scrollEnabled = false
+        
+
         
         let initialLocation = CLLocation(latitude: Double(tripObj.lat), longitude: Double(tripObj.long))
         dispatch_async(dispatch_get_main_queue()) {
