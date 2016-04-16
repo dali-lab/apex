200441bf5428208fe4db3c169a6865c18104084f
Jason Feng <jason21feng@gmail.com>
2016-04-16 11:59:21 -0400

    Add login functionality

---
+++ Apex/Apex/LoginViewController.swift
@@ -0,0 +1,49 @@
+//
+//  LoginViewController.swift
+//  Apex
+//
+//  Created by Jason Feng on 4/16/16.
+//  Copyright © 2016 DALI. All rights reserved.
+//
+
+import Firebase
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    let ref = Firebase(url: "https://apexdatabase.firebaseio.com")
+    let showHome = "showHome"
+    
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var login: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onClickLogin(sender: AnyObject) {
+        ref.authUser(email.text, password: password.text,
+                     withCompletionBlock: { (error, auth) in
+                        
+        })
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        ref.observeAuthEventWithBlock { (authData) -> Void in
+            if authData != nil {
+                self.performSegueWithIdentifier(self.showHome, sender: nil)
+            }
+        }
+    }
+
+}
