6702d6ac14fd558665bae90fc9d658e916b9d3aa
Armin Mahban <armin17@cs.dartmouth.edu>
2016-04-16 10:26:08 -0400

    Armin: Starting Home TableView

---
+++ Apex/Apex/FeedTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  FeedTableViewCell.swift
+//  Apex
+//
+//  Created by ArminM on 4/16/16.
+//  Copyright © 2016 DALI. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var picture: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var dateTime: UILabel!
+    @IBOutlet weak var registration: UILabel!
+    @IBOutlet weak var descriptionText: UILabel!
+    @IBOutlet weak var icon: UIImageView!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Apex/Apex/HomeFeedViewController.swift
@@ -0,0 +1,55 @@
+//
+//  HomeFeedViewController.swift
+//  Apex
+//
+//  Created by ArminM on 4/16/16.
+//  Copyright © 2016 DALI. All rights reserved.
+//
+
+import UIKit
+
+class HomeFeedViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.estimatedRowHeight = 208
+        self.tableView.rowHeight = (UITableViewAutomaticDimension)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("FeedCell", forIndexPath: indexPath) as! FeedTableViewCell
+        
+        
+        return cell
+    }
+//    
+//    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+//        if section == 0 { return CGFloat.min }
+//        return 6
+//    }
+//    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+//        return CGFloat.min
+//    }
+
+}
